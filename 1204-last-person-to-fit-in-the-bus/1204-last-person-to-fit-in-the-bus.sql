aae515c0d28bbd83b4b0b8855f077b9b2ff64e2f
Randhir Kumar <86366172+Mrghostraj@users.noreply.github.com>
2026-08-28 13:18:36 +0530

    Time: 702 ms (99.48%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -4,8 +4,7 @@ FROM (
     SELECT * ,
     SUM(weight) OVER (ORDER BY turn) AS total_weight
 FROM Queue
-ORDER BY turn
 ) t
 WHERE t.total_weight <=1000
-ORDER by t.total_weight DESC
+ORDER by t.turn DESC
 LIMIT 1;
