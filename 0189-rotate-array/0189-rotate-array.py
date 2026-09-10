cdb084bbf2cb13db8f3331894f3b41fdcd6f3118
Randhir Kumar <86366172+Mrghostraj@users.noreply.github.com>
2026-09-10 11:34:12 +0530

    Time: 163 ms (13.41%), Space: 28.2 MB (6.87%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,17 +1,17 @@
-class Solution(object):
-    def reverse(self, arr, low, high):
-        while low < high:
-            arr[low], arr[high] = arr[high], arr[low]
-            low +=1
-            high -=1
-        return arr
+class Solution(object): 
+    def reverser(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start+=1
+            end-=1
+        return nums
 
     def rotate(self, nums, k):
-        self.reverse(nums, 0, len(nums)-1)
-        self.reverse(nums, 0, k-1)
-        self.reverse(nums, k, len(nums)-1)
-
-        
+        k = k%len(nums)
+        self.reverser(nums, 0, len(nums)-1)
+        self.reverser(nums, 0, k-1)
+        self.reverser(nums, k, len(nums)-1)
+        return nums
         
 
         
